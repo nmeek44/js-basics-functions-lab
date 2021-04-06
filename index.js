aa5e7ba16bf85fef559c1dd5c020c78c46f1cd8a
nmeek44 <nmeek44@gmail.com>
2021-04-06 09:41:31 -0400

    commit tree abfb1a30a4280f846fbccc2feaad4c57a9afefc9

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solution in this file!
+let hq = 42
+let numFeetToBlocks = 264
+function distanceFromHqInBlocks(pickupLocation) {
+    let blocksAboveHq = (`${pickupLocation}` - `${hq}`);
+    let blocksBelowHq = (`${hq}` - `${pickupLocation}`);
+    if (blocksAboveHq > 0) {
+        console.log(blocksAboveHq);
+        return blocksAboveHq;
+    } else {
+        console.log(blocksBelowHq);
+        return blocksBelowHq;
+    }
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    return distanceFromHqInBlocks(pickupLocation) * `${numFeetToBlocks}`
+}
+
+function distanceTravelledInFeet(pickupLocation, dropOffLocation) {
+    if (`${pickupLocation}` > `${dropOffLocation}`)  {
+        return (`${pickupLocation}` - `${dropOffLocation}`) * `${numFeetToBlocks}`;
+    } else {
+        return (`${dropOffLocation}` - `${pickupLocation}`) * `${numFeetToBlocks}`;
+    }
+}
